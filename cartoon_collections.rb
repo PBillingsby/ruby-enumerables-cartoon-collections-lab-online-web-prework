902338e28a311dc8d844e2bec549a0cd1887721c
Phil Billingsby <paulbillingsby@gmail.com>
2019-09-18 18:10:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,13 +13,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.each do |name|
-    if name.length >= 4
-      return true
-    else
-     return false
-    end
-  end
+  array.any? {|a| a.length > 4}
 end
 
 def find_the_cheese# code an argument here
