aad90c78a52b125c7382d1d37b730d88b1c2f0e2
Phil Billingsby <paulbillingsby@gmail.com>
2019-09-18 17:52:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,25 @@
-def roll_call_dwarves# code an argument here
+
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    p "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map! {|array| array.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.each do |name|
+    if name.length > 4
+      puts true
+    else
+      p false
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
